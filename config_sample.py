a1c8e69bfccb8de5c390099cab9dd9aaa8e0b285
Vincent Pretre <vincent.pretre@gmail.com>
2014-11-15 17:27:25 +0100

    Errors in config sample

--- config_sample.py
+++ config_sample.py
@@ -1,7 +1,7 @@
 import jobs
 
 # Time to wait after all jobs are checked. In seconds
-SLEEP_TIME = 3
+SLEEP_TIME = 30
 
 # The list of monitored jobs
 JOBS = [
@@ -10,7 +10,7 @@ JOBS = [
 #  A Jenkins job, with authentication
 #  jobs.ci.Jenkins(2, 'My secret project', 'secret-project', 'https://myjenkins.example.com', {'username': 'john', 'password': 'doe'}),
 #  A public Travis-CI job (private ones not handled yet)
-#  jobs.ci.TravisCi(3, 'Job sensors', 'vincent-psarga/job-sensors')
+#  jobs.ci.TravisCI(3, 'Job sensors', 'vincent-psarga/job-sensors')
 #  A website: monitors the time to answer (note: does not take into account CSS, assets etc, simply get the HTML page)
 #  jobs.response.Response(4, 'My website', 'http://www.example.com')
 ]
