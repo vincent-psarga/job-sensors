f023d33eeeb646e181920b990f0ea832b943f560
Vincent Pretre <v.pretre@smartesting.com>
2014-11-21 17:13:08 +0100

    Styled web page a bit, still ugly but does not make the eyes bleed anymore

--- static/js/job-sensors.js
+++ static/js/job-sensors.js
@@ -1,19 +1,23 @@
 (function ($) {
   function get_job_container(job) {
     if ($('#job-' + job.id).length == 0) {
-      $('#jobs').append('<div class="job" id="job-' + job.id + '"><h1></h1><span class="status"></span><span class="author"></span></div>')
+      $('#jobs').append('<div class="job" id="job-' + job.id + '"><h1></h1><span class="status"></span><span class="author"></span><div class="statusColor"></div></div>')
     }
 
     return $('#job-' + job.id);
   }
 
   function update_job_display(job) {
-    var container = get_job_container(job);
+    var container = get_job_container(job),
+      color = tinycolor('#' + job.status.color.color);
 
     container.find('h1').html(job.name);
     container.find('.status').html(job.status.value);
     container.find('.author').html(job.status.author);
-    container.css({'background-color': '#' + job.status.color.color});
+    container.find('.statusColor').css({'background-color': color.toRgbString()});
+
+    color.setAlpha(0.1);
+    container.css({'background-color': color.toRgbString()});
   }
 
   function get_jobs_status() {
